ba254c4b9e010a0088fb5bf26cf4676b83e0a550
Derlyi <dvhuelgos@soy.sena.edu.co>
2023-07-24 22:23:24 -0500

    se inicia JV

---
+++ js/script.js
@@ -0,0 +1,48 @@
+//alert("Analisis y Desarrollo de Software")
+//document.write("Analisis y Desarrollo de Software")
+//console.log("Analisis y Desarrollo de Software")
+
+//para comentar en una linea de codigo 
+/* este tipo de comentario se trabaja en diferentes lineas de codigo*/
+
+//document.write("Analisis y Desarrollo de Software")
+
+//declarar variables var
+
+//var edad=33;
+//const nom="Derly";
+//let num=2.5;
+//booleano='ADSO';
+
+/*unir dos cadenas de texto
+const nom="Derly"
+const ape='Huelgos'
+alert("Mi nombre es "+nom+" y mi apellido es "+ape) */
+
+/* operadores aritmeticos
+const num1=5
+const num2=3
+const div=num1/num2
+alert("La división es: "+div)*/
+
+//let nom=prompt("Escriba el nombre")
+//alert("Mi nombre es: "+nom)
+
+/*mostrar en una alerta con los nombres y apellidos completos, edad, peso y direccion*/
+
+let nom=prompt("Escriba su nombre completo")
+let ape=prompt("Escriba su apellido completo")
+let edad=prompt("Escriba su edad")
+let peso=prompt("Escriba su peso")
+let direccion=prompt("Escriba su dirección")
+alert("Mi nombre es "+nom+ " y mi apellido es "+ape+", tengo "+edad+" años de edad, peso "+peso+" kilos y mi direccion es: "+direccion)
+
+//console.log(typeof num); typeof es para definir el tipo de dato
+
+//console.log("Mi edad es:"+edad); //simbolo "+" va a funcionar para concatenar
+//alert("Mi edad es:"+edad);
+//document.write("Mi edad es:+edad");
+
+//const edad2=22;
+
+//let edad3=33;
